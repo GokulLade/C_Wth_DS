daea0db38435072595b35d9e6b6c8fbab4a62b23
Gokul <gokullade89@gmail.com>
2024-06-09 19:28:43 +0530

    Program Update

--- 1.Operators/8.Increment_DecrementOp/Incre_Decre4.c
+++ 1.Operators/8.Increment_DecrementOp/Incre_Decre4.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 void main()
 {
-    int a=2,b;
+    int a=2;
     a=a++ + ++a;
+    printf("%d\n",a);
 
-    b=++a * --a + a++ / ++a;
+    int b=++a * --a + a++ / ++a; 
 
 
-    printf("%d %d",a,b);
+    printf("%d \n %d",a,b);
 }
---
+++ 8.Control_Statements/2.Conditional_Branching/Switch_Statement/switch3.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+void main()
+{
+    if(1==1)
+    {
+        if(1==0);
+         printf("hello");
+    
+    }
+}
