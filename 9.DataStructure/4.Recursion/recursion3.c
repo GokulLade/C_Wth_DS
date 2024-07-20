84f002b20a1cc9a7a5c3b486dabb7c0880c32bbf
Gokul <gokullade89@gmail.com>
2024-07-20 12:28:09 +0530

    Recursion Program

---
+++ 9.DataStructure/4.Recursion/recursion3.c
@@ -0,0 +1,21 @@
+// Finding digital sum using recursion
+#include <stdio.h>
+int s = 0; /* global var*/
+int dsum(long n)
+
+{
+    if (n != 0)
+    {
+        s = s + n % 10;
+        dsum(n / 10);
+    }
+    return s;
+}
+void main()
+{
+    long n;
+    printf("Enter a no");
+    scanf("%ld", &n);
+
+    printf("%ld digital sum = %d", n, dsum(n));
+}
